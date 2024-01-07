04ec66623bfa3aacca5f211a260ee993d0835b51
Junital <2974033534@qq.com>
2024-01-07 19:42:14 +0800

    second commit P6206 AC

--- Day19/P6206inLuogu.cpp
+++ Day19/P6206inLuogu.cpp
@@ -3,6 +3,7 @@
  * 要求：输入一个数，每次数发生一次变换（奇数就*3+1；偶数就/2），计算最后变换到1的次数。
  * 作者：Junital
  * 思路：模拟（不知道时间能不能过）
+ * 纠正：要换成long long，还是存在溢出的可能的，这个是我没意识到。
  * 函数：OddGame用来模拟变换。
  ***********************************************************/
 
@@ -10,10 +11,13 @@
 
 using namespace std;
 
+#define rep(i, s, k) for (int i = s; i <= k; i++) // 快速循环
+
 /* 输入一个数字，返回变换的次数 */
-int OddGame(int Num)
+long long OddGame(long long Num)
 {
     int Times = 0; // 记录变换的次数
+    // cout << Num << endl;
     while (Num != 1)
     {
         if (Num % 2 == 1) // 如果为奇数 *3+1
@@ -31,7 +35,7 @@ int OddGame(int Num)
 
 int main()
 {
-    int Num;
+    long long Num;
     cin >> Num;
     cout << OddGame(Num) << endl;
     return 0;
