dc741b665d78f4cc467637b313dfe43a3a62ad25
Junital <2974033534@qq.com>
2024-04-08 10:31:40 +0800

    first commit P5714 AC

---
+++ Day104/P5714inLuogu.cpp
@@ -0,0 +1,46 @@
+/***********************************************
+ * 题目：【深基3.例7】肥胖问题
+ * 描述：输入身高、体重，判断是否肥胖。
+ * 思路：简单计算。
+ * 对象：BMI计算BMI值，并提供上下限。
+ * 注意：注意精度。
+ ***********************************************/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class BMI
+{
+public:
+    const double HighNormal = 24;
+    const double LowNormal = 18.5;
+
+    double Calculate(double Weight, double Height)
+    {
+        return Weight / (Height * Height);
+    }
+};
+
+int main()
+{
+    BMI B;
+    double weight, height;
+    cin >> weight >> height;
+
+    double bmi = B.Calculate(weight, height);
+
+    if (bmi >= B.HighNormal)
+    {
+        cout << bmi << endl;
+        cout << "Overweight" << endl;
+    }
+    else if (bmi < B.LowNormal)
+    {
+        cout << "Underweight" << endl;
+    }
+    else
+    {
+        cout << "Normal" << endl;
+    }
+}
