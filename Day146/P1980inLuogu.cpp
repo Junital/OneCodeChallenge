fc4516b3e6537d99e11694801a9e34516d17653d
Junital <2974033534@qq.com>
2024-05-22 17:32:43 +0800

    first commit P1980 WA

---
+++ Day146/P1980inLuogu.cpp
@@ -0,0 +1,60 @@
+/******************************************************
+ * 题目：[NOIP2013 普及组] 计数问题
+ * 描述：对数字出现的字数进行统计。
+ * 思路：从个位、十位、百位依次统计。
+ * 对象：NumberStatistic进行数字统计。
+ * 注意：逻辑完备性。
+ ******************************************************/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class DigitStatistic
+{
+private:
+public:
+    /* 输入数字个数和需要统计的数字。 */
+    int statistics(int Num, int Digit)
+    {
+        assert(Digit >= 0 && Digit <= 9);
+
+        int Group = 1; // 一个组的大小
+        int Sum = 0;   // 统计
+
+        /* 以数字个数为728，数字为7为例 */
+        while (Group <= Num)
+        {
+            Sum += Num / (Group * 10) * Group; // 统计720中的7、700中的70和7000中的700
+
+            if (Num >= Group * 10) // 统计720~728中的7、700~728中的70
+            {
+                int Remainder = Num % (Group * 10);
+                if (Remainder >= Digit * Group && Remainder != 0)
+                {
+                    Sum += Group;
+                }
+            }
+            else if (Num >= Group * Digit) // 统计700~728中的700。
+            {
+                Sum += (Num - Group * Digit + 1);
+            }
+            // cout << Sum << endl;
+
+            Group *= 10;
+        }
+
+        return Sum;
+    }
+};
+
+int main()
+{
+    DigitStatistic ds;
+    int Num, Digit;
+
+    cin >> Num >> Digit;
+
+    cout << ds.statistics(Num, Digit) << endl;
+    return 0;
+}
