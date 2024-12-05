332550a0e9053fb1a4c0ed28a3ca2aefa9b1934c
Junital <2974033534@qq.com>
2024-12-05 11:07:31 +0800

    first commit B3906 AC

---
+++ Day339/B3906inLuogu.cpp
@@ -0,0 +1,58 @@
+/*****************************************************************
+ * 题目：[语言月赛 202312] 禁止在 int 乘 int 时不开 long long
+ * 描述：判断两个数相乘是否会溢出。
+ * 思路：numeric_limits来判断。
+ * 空间：JudgementAids模拟辅助判断库。
+ * 注意：注意提高可拓展性。
+ ****************************************************************/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+namespace JudgementAids
+{
+    template <typename T>
+    bool will_multiply_overflow(T a, T b)
+    {
+        if (a == 0 || b == 0)
+        {
+            return false; // 任何数乘以0都不会溢出
+        }
+
+        if (a > 0 && b > 0)
+        {
+            return a > numeric_limits<T>::max() / b;
+        }
+        else if (a < 0 && b < 0)
+        {
+            return a < numeric_limits<T>::max() / b;
+        }
+        else
+        {
+            return a > numeric_limits<T>::min() / b ||
+                   a < numeric_limits<T>::max() / b;
+        }
+    }
+};
+
+int main()
+{
+    int start1, end1;
+    int start2, end2;
+    cin >> start1 >> end1;
+    cin >> start2 >> end2;
+
+    if (JudgementAids::will_multiply_overflow(start1, start2) ||
+        JudgementAids::will_multiply_overflow(start1, end2) ||
+        JudgementAids::will_multiply_overflow(end1, start2) ||
+        JudgementAids::will_multiply_overflow(end1, end2))
+    {
+        cout << "long long int" << endl;
+    }
+    else
+    {
+        cout << "int" << endl;
+    }
+    return 0;
+}
