8c9879d98d036b86f70ad7ee2d7e01631adb4c44
Junital <2974033534@qq.com>
2025-01-12 10:33:40 +0800

    first commit B4064 AC

---
+++ Day377/B4064inLuogu.cpp
@@ -0,0 +1,38 @@
+/*****************************************************************
+ * 题目：[GESP202412 二级] 寻找数字
+ * 描述：问数字能否被四次开根号。
+ * 思路：
+ * 对象：DigitSum模拟数位求和。
+ * 注意：注意数据范围。
+ ****************************************************************/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define rep(i, s, k) for (int i = s; i <= k; i++) // 定义快速循环
+
+int main()
+{
+    int query_num;
+    const double exponent = 0.25;
+    cin >> query_num;
+
+    rep(i, 1, query_num)
+    {
+        int Num;
+        cin >> Num;
+
+        int X = pow(Num, exponent);
+
+        if (Num == (int)pow(X, 1 / exponent))
+        {
+            cout << X << endl;
+        }
+        else
+        {
+            cout << "-1" << endl;
+        }
+    }
+    return 0;
+}
