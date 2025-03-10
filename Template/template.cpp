31d47ef0d37d7908c796b446d649ac4007952c60
Junital <2974033534@qq.com>
2025-03-10 10:12:18 +0800

    first commit B3764 AC

--- Template/template.cpp
+++ Template/template.cpp
@@ -14,6 +14,7 @@ namespace Tools
 {
 #define rep(i, s, k) for (int i = s; i <= k; i++) // 定义快速循环
 #define ll long long                              // 定义长整型
+#define ull unsigned long long                    // 定义无符号长整型
 #define div_up(a, b) (a) / (b) + ((a) % (b) != 0) // 定义 上整除
 };
 
