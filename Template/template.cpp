756c7bc8c1cd905b9a7f65223c4ccd53eb07a08c
Junital <2974033534@qq.com>
2025-03-31 11:16:22 +0800

    first commit P9198 AC

--- Template/template.cpp
+++ Template/template.cpp
@@ -13,12 +13,28 @@ using namespace std;
 namespace Tools
 {
 #define rep(i, s, k) for (int i = s; i <= k; i++) // 定义快速循环
+#define per(i, s, k) for (int i = s; i >= k; i--) // 定义逆向循环
 #define ll long long                              // 定义长整型
 #define ull unsigned long long                    // 定义无符号长整型
 #define pii pair<int, int>                        // 定义pair
 #define vi vector<int>                            // 定义vector
 #define i64 int64_t                               // 定义int64
 #define div_up(a, b) (a) / (b) + ((a) % (b) != 0) // 定义 上整除
+    /* 四舍五入 */
+    int Approximate(double Num)
+    {
+        double Ceil = ceil(Num);
+        double Floor = floor(Num);
+
+        if (Ceil - Num <= Num - Floor)
+        {
+            return (int)Ceil;
+        }
+        else
+        {
+            return (int)Floor;
+        }
+    }
 };
 
 using namespace Tools;
