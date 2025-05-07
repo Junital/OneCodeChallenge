6650b2f4e0f8ff8b74c9582eb8abf878d1a2d515
Junital <2974033534@qq.com>
2025-05-07 11:02:56 +0800

    first commit B2127 AC

--- Template/template.kt
+++ Template/template.kt
@@ -6,6 +6,25 @@
  * 注意：
  ****************************************************************/
 
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.StringTokenizer
+
+// 全局工具
+private val reader = BufferedReader(InputStreamReader(System.`in`))
+private var tokenizer = StringTokenizer("")
+
+// 不断从 reader 读取行，然后往 tokenizer 塞
+private fun next(): String {
+    while (!tokenizer.hasMoreTokens()) {
+        tokenizer = StringTokenizer(reader.readLine() ?: "")
+    }
+    return tokenizer.nextToken()
+}
+
+// 读取一个 Int
+private fun nextInt(): Int = next().toInt()
+
 fun main(args: Array<String>) {
     
 }
