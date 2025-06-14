d6bf62e7f70fa6147ea8b9ad239163021075d647
Junital <2974033534@qq.com>
2025-06-14 23:05:22 +0800

    first commit B4312 AC

---
+++ Day529/B4312inLuogu.kt
@@ -0,0 +1,57 @@
+/*****************************************************************
+ * 题目：[语言月赛 202504] 炸鸡奶茶
+ * 描述：小明每周会过疯狂星期四，问几天后消费多少。
+ * 思路：简单计算。
+ * 空间：无。
+ * 注意：无。
+ ****************************************************************/
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.StringTokenizer
+
+// 全局工具
+private val reader = BufferedReader(InputStreamReader(System.`in`))
+private var tokenizer = StringTokenizer("")
+
+// 不断从 reader 读取行，然后往 tokenizer 塞
+private fun next(): String {
+    while (!tokenizer.hasMoreTokens()) {
+        tokenizer = StringTokenizer(reader.readLine() ?: "")
+    }
+    return tokenizer.nextToken()
+}
+
+// 读取一个 Int
+private fun nextInt(): Int = next().toInt()
+
+/**
+ * 批量读取 n 个 Int，返回 List<Int>
+ * 可以和解构声明一起使用：
+ * val (x,y,z,w) = nextInts(4)
+ */
+fun nextInts(n: Int): List<Int> = List(n) { nextInt() }
+
+// 读取一个 Long
+private fun nextLong(): Long = next().toLong()
+
+// 读取一个 Double
+private fun nextDouble(): Double = next().toDouble()
+
+// 读取一个 Char
+private fun nextChar(): Char = next().first()
+
+/**
+ * 把 Pair<A,B> 转成 "$first<sep>$second" 的字符串
+ */
+fun <A, B> Pair<A, B>.joinToString(
+    separator: CharSequence = ", "
+): String = listOf(first, second)
+    .joinToString(separator) { it.toString() }
+
+fun main(args: Array<String>) {
+    val weekDay = 7
+    val (friedChicken, milkTea, followingDays) = nextInts(3)
+
+    println(followingDays / weekDay * (friedChicken * 2 + milkTea))
+}
