b4798fa8322d3bc4ee83178bbb80723b7c3f43aa
Junital <2974033534@qq.com>
2025-06-25 18:46:18 +0800

    first commit B4155 AC

--- Template/IntExtension.kt
+++ Template/IntExtension.kt
@@ -29,9 +29,27 @@ fun Int.sumOfDivisors(): Int = divisors().sum()
 fun Int.toDigitList(): List<Int> =
     this.absoluteValue.toString().map { it - '0' }
 
-
 /**
  * 向上整除：a / b，如果有余数则结果再加 1
  */
 fun Int.divUp(b: Int): Int =
     this / b + if (this % b != 0) 1 else 0
+
+/**
+ * 对Int进行除法，输出商的小数部分的数字数组。
+ */
+fun Int.fractionalDigits(divisor: Int, vararg positions: Int): List<Int> {
+    val digits =  buildList {
+        var remainder = this@fractionalDigits % divisor
+
+        repeat(positions.maxOrNull() ?: 0){
+            remainder *= 10
+            add(remainder / divisor)
+            remainder %= divisor
+        }
+    }
+
+    return positions.map { pos ->
+        digits[pos - 1]
+    }
+}
