ccd965a1f55e4e39bff530fc385578fe77c3e2d7
Junital <2974033534@qq.com>
2025-06-26 22:42:18 +0800

    first commit B4230 AC

---
+++ Day541/B4230inLuogu.kt
@@ -0,0 +1,57 @@
+/*****************************************************************
+ * 题目：[四川青少年 C++ 算法设计大赛 2024] 4-5 年级组第 1 题
+ * 描述：问能换多少钱。
+ * 思路：除法 + 取模
+ * 空间：无。
+ * 注意：无。
+ ****************************************************************/
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.StringTokenizer
+
+// 全局工具
+private val reader = BufferedReader(InputStreamReader(System.`in`))
+private var tokenizer = StringTokenizer("")
+
+// 不断从 reader 读取行，然后往 tokenizer 塞
+private fun next(): String {
+    while (!tokenizer.hasMoreTokens()) {
+        tokenizer = StringTokenizer(reader.readLine() ?: "")
+    }
+    return tokenizer.nextToken()
+}
+
+// 读取一个 Int
+private fun nextInt(): Int = next().toInt()
+
+/**
+ * 批量读取 n 个 Int，返回 List<Int>
+ * 可以和解构声明一起使用：
+ * val (x,y,z,w) = nextInts(4)
+ */
+fun nextInts(n: Int): List<Int> = List(n) { nextInt() }
+
+// 读取一个 Long
+private fun nextLong(): Long = next().toLong()
+
+// 读取一个 Double
+private fun nextDouble(): Double = next().toDouble()
+
+// 读取一个 Char
+private fun nextChar(): Char = next().first()
+
+/**
+ * 把 Pair<A,B> 转成 "$first<sep>$second" 的字符串
+ */
+fun <A, B> Pair<A, B>.joinToString(
+    separator: CharSequence = ", "
+): String = listOf(first, second)
+    .joinToString(separator) { it.toString() }
+
+fun main(args: Array<String>) {
+    val price = 5
+    val (num, group) = nextInts(2)
+
+    println("${num / group * price} ${num % group}")
+}
