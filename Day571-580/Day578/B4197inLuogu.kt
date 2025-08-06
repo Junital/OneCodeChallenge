10538ac893073c0e33839bcfe64f8e9865d7484a
Junital <2974033534@qq.com>
2025-08-06 12:25:53 +0800

    first commit P8834 AC

---
+++ Day571-580/Day578/B4197inLuogu.kt
@@ -0,0 +1,73 @@
+/*****************************************************************
+ * 题目：[常州市赛 2020] 欢迎
+ * 描述：根据编号，输出对应长度的欢迎界面。
+ * 思路：计算字符串长度。
+ * 空间：无。
+ * 注意：无。
+ ****************************************************************/
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.io.EOFException
+import java.util.StringTokenizer
+
+// 全局工具
+private val reader = BufferedReader(InputStreamReader(System.`in`))
+private var tokenizer = StringTokenizer("")
+
+// 不断从 reader 读取行，然后往 tokenizer 塞
+private fun next(): String {
+    while (!tokenizer.hasMoreTokens()) {
+        tokenizer = StringTokenizer(reader.readLine() ?: "")
+    }
+    return tokenizer.nextToken()
+}
+
+/**
+ * 批量读取 n 个 String，返回 List<String>
+ * 可以和解构声明一起使用：
+ * val (x,y,z,w) = nextStrings(4)
+ */
+fun nextStrings(n: Int): List<String> = List(n) { next() }
+
+// 读取一个 Int
+private fun nextInt(): Int = next().toInt()
+
+/**
+ * 批量读取 n 个 Int，返回 List<Int>
+ * 可以和解构声明一起使用：
+ * val (x,y,z,w) = nextInts(4)
+ */
+fun nextInts(n: Int): List<Int> = List(n) { nextInt() }
+
+// 读取一个 Long
+private fun nextLong(): Long = next().toLong()
+
+/**
+ * 批量读取 n 个 Long，返回 List<Long>
+ * 可以和解构声明一起使用：
+ * val (x,y,z,w) = nextLongs(4)
+ */
+fun nextLongs(n: Int): List<Long> = List(n) { nextLong() }
+
+// 读取一个 Double
+private fun nextDouble(): Double = next().toDouble()
+
+// 读取一个 Char
+private fun nextChar(): Char = next().first()
+
+/**
+ * 把 Pair<A,B> 转成 "$first<sep>$second" 的字符串
+ */
+fun <A, B> Pair<A, B>.joinToString(
+    separator: CharSequence = ", "
+): String = listOf(first, second)
+    .joinToString(separator) { it.toString() }
+
+fun main(args: Array<String>) {
+    val welcome = "*Welcome,ContestantNo." + next() + "!*"
+
+    println("*".repeat(welcome.length))
+    println(welcome)
+    println("*".repeat(welcome.length))
+}
