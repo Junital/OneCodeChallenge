cf7cddbba28299e70b7522cf7d87efe427a38bfc
Junital <2974033534@qq.com>
2025-09-23 22:57:21 +0800

    first commit P8627 AC

---
+++ Day620/P8627inLuogu.kt
@@ -0,0 +1,87 @@
+/*****************************************************************
+ * 题目：[蓝桥杯 2015 省 A] 饮料换购
+ * 描述：三个瓶盖换一个饮料，喝完后瓶盖也会被喝完，问最多可以喝多少饮料
+ * 思路：每次喝完后，瓶盖数加一，饮料数减一，直到瓶盖数小于三
+ * 函数：drinkChange模拟饮料换购。
+ * 注意：无。
+ ****************************************************************/
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.io.EOFException
+import java.util.StringTokenizer
+
+// 全局工具
+private val reader = BufferedReader(InputStreamReader(System.`in`))
+private var tokenizer = StringTokenizer("")
+
+// 不断从 reader 读取行，然后往 tokenizer 塞
+private fun next(): String {
+    while (!tokenizer.hasMoreTokens()) {
+        tokenizer = StringTokenizer(reader.readLine() ?: "")
+    }
+    return tokenizer.nextToken()
+}
+
+/**
+ * 批量读取 n 个 String，返回 List<String>
+ * 可以和解构声明一起使用：
+ * val (x,y,z,w) = nextStrings(4)
+ */
+fun nextStrings(n: Int): List<String> = List(n) { next() }
+
+// 读取一个 Int
+private fun nextInt(): Int = next().toInt()
+
+/**
+ * 批量读取 n 个 Int，返回 List<Int>
+ * 可以和解构声明一起使用：
+ * val (x,y,z,w) = nextInts(4)
+ */
+fun nextInts(n: Int): List<Int> = List(n) { nextInt() }
+
+// 读取一个 Long
+private fun nextLong(): Long = next().toLong()
+
+/**
+ * 批量读取 n 个 Long，返回 List<Long>
+ * 可以和解构声明一起使用：
+ * val (x,y,z,w) = nextLongs(4)
+ */
+fun nextLongs(n: Int): List<Long> = List(n) { nextLong() }
+
+// 读取一个 Double
+private fun nextDouble(): Double = next().toDouble()
+
+// 读取n个Double
+fun nextDoubles(n: Int): List<Double> = List(n) { nextDouble() }
+
+// 读取一个 Char
+private fun nextChar(): Char = next().first()
+
+/**
+ * 把 Pair<A,B> 转成 "$first<sep>$second" 的字符串
+ */
+fun <A, B> Pair<A, B>.joinToString(
+    separator: CharSequence = ", "
+): String = listOf(first, second)
+    .joinToString(separator) { it.toString() }
+
+/**
+ * 饮料换购
+ */
+fun drinkChange(capacity: Int): Int {
+    var capacity = capacity
+    var count = capacity
+    while (capacity >= 3) {
+        count += capacity / 3
+        capacity = capacity / 3 + capacity % 3
+    }
+    return count
+
+}
+
+fun main(args: Array<String>) {
+    val capacity = nextInt()
+    println(drinkChange(capacity))
+}
