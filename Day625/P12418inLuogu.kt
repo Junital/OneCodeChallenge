b05a736c11102ecc5895e741850519f58a7f86e4
Junital <2974033534@qq.com>
2025-09-28 16:33:15 +0800

    first commit P12418 AC

---
+++ Day625/P12418inLuogu.kt
@@ -0,0 +1,84 @@
+/*****************************************************************
+ * 题目：【MX-X12-T1】「ALFR Round 5」地铁
+ * 描述：问最少需要几根直线，才能联通网格内的所有点。
+ * 思路：1 + (min(n, m))
+ * 函数：minLinesForAMesh
+ * 注意：无。
+ ****************************************************************/
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.io.EOFException
+import java.util.StringTokenizer
+
+// 全局工具
+private val reader = BufferedReader(InputStreamReader(System.`in`))
+private var tokenizer = StringTokenizer("")
+
+// 不断从 reader 读取行，然后往 tokenizer 塞
+private fun next(): String {
+    while (!tokenizer.hasMoreTokens()) {
+        tokenizer = StringTokenizer(reader.readLine() ?: "")
+    }
+    return tokenizer.nextToken()
+}
+
+/**
+ * 批量读取 n 个 String，返回 List<String>
+ * 可以和解构声明一起使用：
+ * val (x,y,z,w) = nextStrings(4)
+ */
+fun nextStrings(n: Int): List<String> = List(n) { next() }
+
+// 读取一个 Int
+private fun nextInt(): Int = next().toInt()
+
+/**
+ * 批量读取 n 个 Int，返回 List<Int>
+ * 可以和解构声明一起使用：
+ * val (x,y,z,w) = nextInts(4)
+ */
+fun nextInts(n: Int): List<Int> = List(n) { nextInt() }
+
+// 读取一个 Long
+private fun nextLong(): Long = next().toLong()
+
+/**
+ * 批量读取 n 个 Long，返回 List<Long>
+ * 可以和解构声明一起使用：
+ * val (x,y,z,w) = nextLongs(4)
+ */
+fun nextLongs(n: Int): List<Long> = List(n) { nextLong() }
+
+// 读取一个 Double
+private fun nextDouble(): Double = next().toDouble()
+
+// 读取n个Double
+fun nextDoubles(n: Int): List<Double> = List(n) { nextDouble() }
+
+// 读取一个 Char
+private fun nextChar(): Char = next().first()
+
+/**
+ * 把 Pair<A,B> 转成 "$first<sep>$second" 的字符串
+ */
+fun <A, B> Pair<A, B>.joinToString(
+    separator: CharSequence = ", "
+): String = listOf(first, second)
+    .joinToString(separator) { it.toString() }
+
+/**
+ * 计算最少需要几根直线，才能联通网格内的所有点。
+ */
+fun minLinesForAMesh(n: Int, m: Int): Int {
+    if(n == 1 || m == 1) return 1
+    return 1 + (minOf(n, m))
+}
+
+fun main(args: Array<String>) {
+    val query = nextInt()
+    repeat(query) {
+        val (n, m) = nextInts(2)
+        println(minLinesForAMesh(n, m))
+    }
+}
