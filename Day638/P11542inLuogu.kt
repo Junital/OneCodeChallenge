bb78d5de54546a2bf688570e9d7d4c7892c02517
Junital <2974033534@qq.com>
2025-10-16 22:08:51 +0800

    first commit P11542 AC

---
+++ Day638/P11542inLuogu.kt
@@ -0,0 +1,91 @@
+/*****************************************************************
+ * 题目：Code+#5 有人吗？
+ * 描述：给定一个数组，有正有负，问是否有一个排序方式，使得前缀和始终非负。
+ * 思路：先正后负。
+ * 函数：classifyNegPos模拟分类正数组和负数组。
+ * 注意：无。
+ ****************************************************************/
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.io.EOFException
+import java.util.StringTokenizer
+
+// 全局工具
+private val reader = BufferedReader(InputStreamReader(System.`in`))
+private var tokenizer = StringTokenizer("")
+
+// 不断从 reader 读取行，然后往 tokenizer 塞
+private fun next(): String {
+    while (!tokenizer.hasMoreTokens()) {
+        tokenizer = StringTokenizer(reader.readLine() ?: "")
+    }
+    return tokenizer.nextToken()
+}
+
+/**
+ * 批量读取 n 个 String，返回 List<String>
+ * 可以和解构声明一起使用：
+ * val (x,y,z,w) = nextStrings(4)
+ */
+fun nextStrings(n: Int): List<String> = List(n) { next() }
+
+// 读取一个 Int
+private fun nextInt(): Int = next().toInt()
+
+/**
+ * 批量读取 n 个 Int，返回 List<Int>
+ * 可以和解构声明一起使用：
+ * val (x,y,z,w) = nextInts(4)
+ */
+fun nextInts(n: Int): List<Int> = List(n) { nextInt() }
+
+// 读取一个 Long
+private fun nextLong(): Long = next().toLong()
+
+/**
+ * 批量读取 n 个 Long，返回 List<Long>
+ * 可以和解构声明一起使用：
+ * val (x,y,z,w) = nextLongs(4)
+ */
+fun nextLongs(n: Int): List<Long> = List(n) { nextLong() }
+
+// 读取一个 Double
+private fun nextDouble(): Double = next().toDouble()
+
+// 读取n个Double
+fun nextDoubles(n: Int): List<Double> = List(n) { nextDouble() }
+
+// 读取一个 Char
+private fun nextChar(): Char = next().first()
+
+/**
+ * 把 Pair<A,B> 转成 "$first<sep>$second" 的字符串
+ */
+fun <A, B> Pair<A, B>.joinToString(
+    separator: CharSequence = ", "
+): String = listOf(first, second)
+    .joinToString(separator) { it.toString() }
+
+/**
+ * 分类正数组和负数组。
+ * param a 输入数组。
+ * return 分类后的数组。
+ */
+fun classifyNegPos(a: List<Int>): Pair<List<Int>, List<Int>> {
+    val neg = a.filter { it < 0 }
+    val pos = a.filter { it >= 0 }
+    return pos to neg
+}
+
+
+fun main(args: Array<String>) {
+    val n = nextInt()
+    val a = nextInts(n)
+    val (pos, neg) = classifyNegPos(a)
+    if(pos.sum() >= -neg.sum()){
+        println("Wo jue de OK")
+    }else{
+        println("Wo jue de bu tai xing")
+    }
+}
