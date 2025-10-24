0c5425a4b3cfdbab8c43f185bae862505360efeb
Junital <2974033534@qq.com>
2025-10-24 23:55:50 +0800

    first commit P7031 AC

---
+++ Day644/P7031inLuogu.kt
@@ -0,0 +1,78 @@
+/*****************************************************************
+ * 题目：[NWRRC 2016] Anniversary Cake
+ * 描述：给定一个矩形，和两个坐标，问如何切出一条线使得两个坐标在不同的部分中。
+ * 思路：直接用固定模式切就行。
+ * 空间：无。
+ * 注意：无。
+ ****************************************************************/
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.io.EOFException
+import java.util.StringTokenizer
+
+// 全局工具
+private val reader = BufferedReader(InputStreamReader(System.`in`))
+private var tokenizer = StringTokenizer("")
+
+// 不断从 reader 读取行，然后往 tokenizer 塞
+private fun next(): String {
+    while (!tokenizer.hasMoreTokens()) {
+        tokenizer = StringTokenizer(reader.readLine() ?: "")
+    }
+    return tokenizer.nextToken()
+}
+
+/**
+ * 批量读取 n 个 String，返回 List<String>
+ * 可以和解构声明一起使用：
+ * val (x,y,z,w) = nextStrings(4)
+ */
+fun nextStrings(n: Int): List<String> = List(n) { next() }
+
+// 读取一个 Int
+private fun nextInt(): Int = next().toInt()
+
+/**
+ * 批量读取 n 个 Int，返回 List<Int>
+ * 可以和解构声明一起使用：
+ * val (x,y,z,w) = nextInts(4)
+ */
+fun nextInts(n: Int): List<Int> = List(n) { nextInt() }
+
+// 读取一个 Long
+private fun nextLong(): Long = next().toLong()
+
+/**
+ * 批量读取 n 个 Long，返回 List<Long>
+ * 可以和解构声明一起使用：
+ * val (x,y,z,w) = nextLongs(4)
+ */
+fun nextLongs(n: Int): List<Long> = List(n) { nextLong() }
+
+// 读取一个 Double
+private fun nextDouble(): Double = next().toDouble()
+
+// 读取n个Double
+fun nextDoubles(n: Int): List<Double> = List(n) { nextDouble() }
+
+// 读取一个 Char
+private fun nextChar(): Char = next().first()
+
+/**
+ * 把 Pair<A,B> 转成 "$first<sep>$second" 的字符串
+ */
+fun <A, B> Pair<A, B>.joinToString(
+    separator: CharSequence = ", "
+): String = listOf(first, second)
+    .joinToString(separator) { it.toString() }
+
+fun main(args: Array<String>) {
+    val (w, h) = nextInts(2)
+    val (ax, ay, bx, by) = nextInts(4)
+    if(ax == bx) {
+        println("0 $ay $w $by")
+    }else {
+        println("${ax} 0 ${bx} $h")
+    }
+}
