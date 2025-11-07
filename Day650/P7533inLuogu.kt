05d2e8aaee7a5f14bfd10321e5024e7a2549a33e
Junital <2974033534@qq.com>
2025-11-07 22:33:18 +0800

    first commit P7533 AC

---
+++ Day650/P7533inLuogu.kt
@@ -0,0 +1,88 @@
+/*****************************************************************
+ * 题目：[COCI 2016/2017 #4] Bridž
+ * 描述：一个字符代表一个数值，问最终的数值求和。
+ * 思路：映射求和。
+ * 函数：无。
+ * 注意：无。
+ ****************************************************************/
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.io.EOFException
+import java.util.StringTokenizer
+
+// 全局工具
+private val reader = BufferedReader(InputStreamReader(System.`in`))
+private var tokenizer = StringTokenizer("")
+
+// 不断从 reader 读取行，然后往 tokenizer 塞
+private fun next(): String {
+    while (!tokenizer.hasMoreTokens()) {
+        tokenizer = StringTokenizer(reader.readLine() ?: "")
+    }
+    return tokenizer.nextToken()
+}
+
+/**
+ * 批量读取 n 个 String，返回 List<String>
+ * 可以和解构声明一起使用：
+ * val (x,y,z,w) = nextStrings(4)
+ */
+fun nextStrings(n: Int): List<String> = List(n) { next() }
+
+// 读取一个 Int
+private fun nextInt(): Int = next().toInt()
+
+/**
+ * 批量读取 n 个 Int，返回 List<Int>
+ * 可以和解构声明一起使用：
+ * val (x,y,z,w) = nextInts(4)
+ */
+fun nextInts(n: Int): List<Int> = List(n) { nextInt() }
+
+// 读取一个 Long
+private fun nextLong(): Long = next().toLong()
+
+/**
+ * 批量读取 n 个 Long，返回 List<Long>
+ * 可以和解构声明一起使用：
+ * val (x,y,z,w) = nextLongs(4)
+ */
+fun nextLongs(n: Int): List<Long> = List(n) { nextLong() }
+
+// 读取一个 Double
+private fun nextDouble(): Double = next().toDouble()
+
+// 读取n个Double
+fun nextDoubles(n: Int): List<Double> = List(n) { nextDouble() }
+
+// 读取一个 Char
+private fun nextChar(): Char = next().first()
+
+/**
+ * 把 Pair<A,B> 转成 "$first<sep>$second" 的字符串
+ */
+fun <A, B> Pair<A, B>.joinToString(
+    separator: CharSequence = ", "
+): String = listOf(first, second)
+    .joinToString(separator) { it.toString() }
+
+fun main(args: Array<String>) {
+    val map = mapOf(
+        // A, K, Q, J 和 X。它们的分值分别为 4,3,2,1,0。
+        'A' to 4,
+        'K' to 3,
+        'Q' to 2,
+        'J' to 1,
+        'X' to 0,
+    )
+
+    val n = nextInt()
+    var sum = 0
+    repeat(n) {
+        val s = next()
+        val subSum = s.sumOf { map[it] ?: 0 }
+        sum += subSum
+    }
+    println(sum)
+}
